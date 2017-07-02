da294cfa568b3f564a7a3e97c142295220fbb294
Mateus de Campos <mateuscampos_nba@hotmail.com>
2017-07-02 19:48:15 -0300

    adding slather to gemfile

---
+++ Brejas/BrejasTests/BeerModelSpec.swift
@@ -0,0 +1,21 @@
+//
+//  BeerModelSpec.swift
+//  Brejas
+//
+//  Created by Mateus Campos on 02/07/17.
+//  Copyright © 2017 Mateus Campos. All rights reserved.
+//
+
+import Quick
+import Nimble
+import ObjectMapper
+
+class BeerModelSpec: QuickSpec {
+    
+    override func spec() {
+        
+        
+        
+    }
+    
+}
